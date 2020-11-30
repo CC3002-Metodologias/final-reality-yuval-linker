db57a2e01c269a0a7e1d0f8d54f9793113c25f23
yuvallinker <yuvallinkerg@gmail.com>
2020-11-30 16:35:36 -0300

    Wait turn y enemy turn tests

--- src/test/java/com/github/ylinker/finalreality/controller/ControllerUtilTest.java
+++ src/test/java/com/github/ylinker/finalreality/controller/ControllerUtilTest.java
@@ -3,18 +3,9 @@ package com.github.ylinker.finalreality.controller;
 import com.github.ylinker.finalreality.model.character.Enemy;
 import com.github.ylinker.finalreality.model.character.ICharacter;
 import com.github.ylinker.finalreality.model.character.IPlayerCharacter;
-import com.github.ylinker.finalreality.model.character.player.common.Engineer;
-import com.github.ylinker.finalreality.model.character.player.common.Knight;
-import com.github.ylinker.finalreality.model.character.player.common.Thief;
-import com.github.ylinker.finalreality.model.character.player.mage.BlackMage;
-import com.github.ylinker.finalreality.model.character.player.mage.IMage;
-import com.github.ylinker.finalreality.model.character.player.mage.WhiteMage;
-import com.github.ylinker.finalreality.model.weapon.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.swing.plaf.synth.SynthTabbedPaneUI;
-import java.util.HashMap;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.Executors;
 
@@ -81,6 +72,58 @@ public class ControllerUtilTest {
         testController.initTurns();
         assertNotNull(testController.getCharacters().get(0).getScheduledExecutor());
         assertNotNull(testController.getEnemies().get(0).getScheduledExecutor());
+    }
+
+    @Test
+    void waitTurnTest() {
+        testController.createEngineer("testEngineer", 32, 39, 49);
+        testController.createEnemy("testEnemy", 83, 49, 20, 20);
+        testController.createKnight("dummy", 22, 43, 43);
+        BlockingQueue<ICharacter> queue = testController.getQueue();
+        IPlayerCharacter engineer = testController.getCharacters().get(0);
+        IPlayerCharacter dummy = testController.getCharacters().get(1);
+        Enemy enemy = testController.getEnemies().get(0);
+        // Add dummy to queue so that turn doesnt begin instantly
+        queue.add(dummy);
+
+        // Schedulers should be null
+        assertNull(engineer.getScheduledExecutor());
+        assertNull(enemy.getScheduledExecutor());
+
+        testController.waitTurn(enemy);
+        testController.waitTurn(engineer);
+        assertNotNull(engineer.getScheduledExecutor());
+        assertNotNull(enemy.getScheduledExecutor());
+        try {
+            Thread.sleep(1100);
+            assertTrue(engineer.getScheduledExecutor().isShutdown());
+            assertFalse(enemy.getScheduledExecutor().isShutdown());
+            Thread.sleep(1000);
+            assertTrue(enemy.getScheduledExecutor().isShutdown());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void testEnemyTurn() {
+        testController.createEnemy("testEnemy", 20, 20, 10, 10);
+        testController.createKnight("testKnight", 20, 10, 10);
+        Enemy enemy = testController.getEnemies().get(0);
+        IPlayerCharacter knight = testController.getCharacters().get(0);
+        testController.waitTurn(enemy);
+        try {
+            Thread.sleep(500);
+            assertEquals(20, testController.getCharacterHealth(knight));
+            Thread.sleep(600);
+            assertEquals(10, testController.getCharacterHealth(knight));
+            Thread.sleep(500);
+            assertEquals(10, testController.getCharacterHealth(knight));
+            Thread.sleep(600);
+            assertEquals(0, testController.getCharacters().size());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
